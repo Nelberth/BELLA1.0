d43c60335c9d9f5ce0699b9543940ac6adfb18e1
Christian Moralesgit config --global user.email <christianmoralesmdb@gmail.com>
2022-02-18 14:25:23 -0400

    tabla de productos lista

--- src/app/inventario/almacen-industria/almacen-industria.component.ts
+++ src/app/inventario/almacen-industria/almacen-industria.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import {TooltipPosition} from '@angular/material/tooltip';
 import { ModalProductoActualizarComponent } from './modal-producto-actualizar/modal-producto-actualizar.component';
+import { ModalProductoEliminarComponent } from './modal-producto-eliminar/modal-producto-eliminar.component';
 import { ModalProductoComponent } from './modal-producto/modal-producto.component';
 @Component({
   selector: 'app-almacen-industria',
@@ -9,7 +10,10 @@ import { ModalProductoComponent } from './modal-producto/modal-producto.componen
   styleUrls: ['./almacen-industria.component.css']
 })
 export class AlmacenIndustriaComponent implements OnInit {
-
+  constructor(public dialog: MatDialog) { }
+  ngOnInit(): void {
+  }
+  
   agregarProducto() {
     const dialogRef = this.dialog.open(ModalProductoComponent);
 
@@ -18,30 +22,19 @@ export class AlmacenIndustriaComponent implements OnInit {
     });
   }
   actualizarProducto(e:Event){
-    
     let a=e.target as Element;
-
-    console.log(a);
+    console.log(e);
     const dialogRef = this.dialog.open(ModalProductoActualizarComponent);
-
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  eliminarProducto(){
+    const dialogRef = this.dialog.open(ModalProductoEliminarComponent);
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
   }
 
-
-
-
-
-
-
-
-
-
-
-  constructor(public dialog: MatDialog) { }
-
-  ngOnInit(): void {
-  }
-
 }
+  
